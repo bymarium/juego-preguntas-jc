7ffe41bf4eae53d011d6ead691fe4bb9ff9ac698
Bymarium <marianamejia.91619@gmail.com>
2022-11-18 23:16:17 -0500

    Terminada la seccion de reglas de juego a traves de una alerta.

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,6 +31,38 @@ const scrollHeader = () => {
 
 window.addEventListener('scroll', scrollHeader);
 
+const alertH = document.getElementById("rulesh");
+if (alertH) {
+  alertH.addEventListener("click", () => {
+    Swal.fire({
+      title: "Reglas de juego",
+      html: "1. El juego consta de 10 preguntas <br/> 2. Disponerá de 60 segundos para cada pregunta <br/> 3. Cada pregunta tendrá dos opciones de respuestas <br/> 4. Deberá seleccionar la que considere correcta y darle click en el boton de enviar <br/> 5. Si la respuesta es correcta se marcará en verde, de lo contrario, se marcará en rojo <br/> 6. Al finalizar el juego se le dará un puntaje que determina si ganó o perdió el juego de preguntas",
+      confirmButtonText: "Entendido",
+      confirmButtonColor: "#ff289b",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  });
+}
 
-
-
+const alertF = document.getElementById("rulesf");
+if (alertF) {
+  alertF.addEventListener("click", () => {
+    Swal.fire({
+      title: "Reglas de juego",
+      html: "1. El juego consta de 10 preguntas <br/> 2. Disponerá de 60 segundos para cada pregunta <br/> 3. Cada pregunta tendrá dos opciones de respuestas <br/> 4. Deberá seleccionar la que considere correcta y darle click en el boton de enviar <br/> 5. Si la respuesta es correcta se marcará en verde, de lo contrario, se marcará en rojo <br/> 6. Al finalizar el juego se le dará un puntaje que determina si ganó o perdió el juego de preguntas",
+      confirmButtonText: "Entendido",
+      confirmButtonColor: "#ff289b",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  });
+}
